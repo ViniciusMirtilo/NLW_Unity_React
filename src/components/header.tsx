09ba59bd23c28821ca3fbdde4501a843696b3fd3
ViniciusMirtilo <vinicius.contato899@hotmail.com>
2024-04-02 17:24:01 -0300

    Project NLW Unity

---
+++ src/components/header.tsx
@@ -0,0 +1,15 @@
+import NLWUnityIcon from '../img/NlW_unity_logo.svg'
+
+export function Header() {
+    return(
+       <div className='flex items-center  gap-5 py-2'>
+            <img src={NLWUnityIcon} />
+
+            <nav className='flex items-center gap-5'>
+                <a href="" className=' font-medium text-sm text-zinc-300'>Eventos</a>
+                <a href="" className=' font-medium text-sm'>Participantes</a>
+            </nav>
+       </div>
+        
+    )
+}
